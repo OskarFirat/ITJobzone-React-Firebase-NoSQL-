75d96af426a7a8248f8b119347e11740fb52983a
Oskar Firat <oskarqq@hotmail.com>
2019-11-02 07:38:03 +0100

    React Nett filene

---
+++ build/precache-manifest.710206d43b6caa3798f3c227730b79b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dceeef2142479aab4765806ea1b75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1307518bbc9a5fedc290",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "211b9ad3a46989af0c3d",
+    "url": "/static/css/main.fb946889.chunk.css"
+  },
+  {
+    "revision": "1307518bbc9a5fedc290",
+    "url": "/static/js/2.1bb31852.chunk.js"
+  },
+  {
+    "revision": "211b9ad3a46989af0c3d",
+    "url": "/static/js/main.2663b009.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3980b9707c2ec3ceb34c9c1efc8408df",
+    "url": "/static/media/bakgrunn111.3980b970.jpg"
+  },
+  {
+    "revision": "1f7ca3e44a34c9fe83f55031fd15c7b6",
+    "url": "/static/media/bakgrunn2222.1f7ca3e4.jpg"
+  },
+  {
+    "revision": "c9d17fa99ff393ac757b42775732c86b",
+    "url": "/static/media/bakgrunn333.c9d17fa9.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
