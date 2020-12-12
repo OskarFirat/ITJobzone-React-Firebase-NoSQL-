e1bc3d53c8d225696dd5bd8f71b60838f3e2b2c4
OskarFirat <43525021+OskarFirat@users.noreply.github.com>
2020-12-12 19:18:17 +0100

    Update Fire.js

--- src/components/kontaktDatabase/Fire.js
+++ src/components/kontaktDatabase/Fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCK4EjHHiV0xUMQQ5TqMbdxGcXNjThrQNY",
+  apiKey: "AIzaSyCK4EjHPiV0xUQQQ5TqMbdxGcXNjThrQNY",
   authDomain: "brukere-864de.firebaseapp.com",
   databaseURL: "https://brukere-864de.firebaseio.com",
   projectId: "brukere-864de",
   storageBucket: "",
   messagingSenderId: "551602662732",
-  appId: "1:551602662732:web:fd583c72c645ffb32e33e9"
+  appId: "1:551602662732:web:fd583c22c645ffb32e33e9"
   };
 const fire = firebase.initializeApp(config);
 export default fire;
